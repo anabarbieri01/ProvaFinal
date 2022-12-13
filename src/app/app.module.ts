524c74eb18d2aece1d0ad3fa3acdb468f7a49625
Ana Barbieri <ana.bbarbieri01@gmail.com>
2022-12-13 08:52:44 -0300

    02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DadosComponent } from './dados/dados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadosComponent
   ],
   imports: [
     BrowserModule
